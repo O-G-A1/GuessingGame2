6db1386d6b149f8d55d08ef30eaff1743b447dc0
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-08-14 14:00:02 +0100

    firstphase

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 let compNumber=parseInt(Math.random()*4)+1;
-let humanGuess=parseInt(prompt("Guess a number"));
+let humanGuess;
 let remark;
-
 let maximumTrials=3;
 let minimumTrials=1;
 
 
-console.log(compNumber);
-console.log(humanGuess);
 
 
-document.getElementById("humanguess").textContent+=humanGuess;
-document.getElementById("Compnumber").textContent+=compNumber;
 
+
+for(minimumTrials=1; minimumTrials<=maximumTrials;minimumTrials++){
+    humanGuess=parseInt(prompt("Guess a number between 1 and 4. " + " You are given " + minimumTrials + " of " + maximumTrials + " trials"));
     if(humanGuess===compNumber){
         alert("Wow! You guessed right.");
+        document.getElementById("humanguess").textContent+=humanGuess;
+        // document.getElementById("humanguess").value;
+        document.getElementById("Compnumber").textContent+=compNumber;
         remark="Congratulations! You guessed right.\n Welcome to stage 2."
         document.getElementById("remark").textContent+=remark;
-    }else{
-        alert("You are wrong!");
-        remark="Sorry, you did not guess right. Try again."
-        document.getElementById("remark").textContent+=remark;
+
+        break;
     }
+    else if(humanGuess!=compNumber){
+            alert("You are wrong!");
+            remark="Sorry, you did not guess right. Try again."
+            document.getElementById("humanguess").textContent+=humanGuess + " - ";
+            document.getElementById("Compnumber").textContent+=compNumber + " - ";
+            document.getElementById("remark").textContent+=remark;
+            let decreasetrials=maximumTrials-minimumTrials;
+        if(minimumTrials===maximumTrials){
+                alert("Game over");
+            }
+       else if(decreasetrials>0){
+                alert("You have " + decreasetrials + " more attempts");
+            }
+        }
+        
     
+
+}
+
+
+
  
     
         
