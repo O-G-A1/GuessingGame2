54322e3e2b7cabf725070e98d8d688d68060427c
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-08-15 08:59:40 +0100

    updated

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ for(minimumTrials=1; minimumTrials<=maximumTrials;minimumTrials++){
         document.getElementById("humanguess").textContent+=humanGuess;
         // document.getElementById("humanguess").value;
         document.getElementById("Compnumber").textContent+=compNumber;
-        remark="Congratulations! You guessed right.\n Welcome to stage 2."
+        remark="Congratulations! You guessed right.\n Welcome to stage 2.";
         document.getElementById("remark").textContent+=remark;
 
         break;
     }
     else if(humanGuess!=compNumber){
-            alert("You are wrong!");
-            remark="Sorry, you did not guess right. Try again."
+            alert("You are wrong, try again!");
             document.getElementById("humanguess").textContent+=humanGuess + " - ";
             document.getElementById("Compnumber").textContent+=compNumber + " - ";
-            document.getElementById("remark").textContent+=remark;
             let decreasetrials=maximumTrials-minimumTrials;
-        if(minimumTrials===maximumTrials){
+            if(minimumTrials===maximumTrials){
                 alert("Game over");
+                remark="Sorry, you did not guess right. Try again."
+                document.getElementById("remark").textContent+=remark;
             }
        else if(decreasetrials>0){
                 alert("You have " + decreasetrials + " more attempts");
